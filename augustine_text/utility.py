410daed5eebf1b9d81eaaf9b975e40c7f7238583
Patrick Shechet <patrick.shechet@gmail.com>
2021-11-26 16:54:10 -0700

    add test for word hashes, nuitka on utility module

--- augustine_text/utility.py
+++ augustine_text/utility.py
@@ -1,4 +1,3 @@
-import collections
 import os
 import re
 import typing as t
@@ -8,16 +7,15 @@ from augustine_text.patterns import GREEDY_WHITESPACE, SENTENCE_END, TOKEN_END
 
 
 def hash_word(word: str) -> int:
-    p, m, pow, val = 31, 1e9 + 9, 1, 0
-
+    pow, val = 1, 0
+    
     def roll(char: str):
         nonlocal val, pow
-        val = (val + (ord(char) - 96) * pow) % m
-        pow = (pow * p) % m
+        val = (val + (ord(char) - 96) * pow) % 1_000_000_009.0
+        pow = (pow * 31) % 1_000_000_009.0
 
     # Rolling forward and then in reverse reduces collisions
-    [roll(char) for char in word]
-    [roll(char) for char in word[::-1]]
+    [roll(char) for char in (word + word[::-1])]
 
     return int(val)
 
@@ -35,32 +33,5 @@ def sentencize(text: str) -> t.List[str]:
 def tokenize(text: str) -> t.List[t.Dict[str, t.Any]]:
     return [w.strip() for w in re.split(TOKEN_END, text) if w.strip()]
 
-
-if __name__ == "__main__":
-    from collections import defaultdict
-    import pathlib
-
-    total_words = []
-
-    path = pathlib.Path("/home/giblesnot/code/augustine-text")
-
-    for p in path.iterdir():
-        if p.is_file() and p.suffix == ".txt":
-            with open(p, encoding="utf-8") as fh:
-                text = fh.read()
-            sentences = sentencize(text)
-            [total_words.extend(tokenize(s)) for s in sentences]
-
-    unique_words = list(set([w.lower() for w in total_words]))
-
-    print(f"total words {len(total_words)}, unique words {len(unique_words)}")
-
-    known = defaultdict(list)
-
-    for w in unique_words:
-        hash = hash_word(w)
-        known[hash].append(w)
-
-    collisions = [v for k, v in known.items() if len(v) > 1]
-    print(f"{len(collisions)*1.00/len(unique_words)}% collisions")
-    # print(collisions)
+# cd .\augustine_text\
+# python -m nuitka --module .\utility.py
--- test/test_words.py
+++ test/test_words.py
@@ -1,16 +1,36 @@
-from ward import test
-from augustine_text.sample_text import words
-from augustine_text.utility import ue_id
+from collections import defaultdict
+from importlib import resources
+
+from augustine_text import data
+from augustine_text.utility import hash_word, sentencize, tokenize, ue_id
+from ward import fixture, test
+import typing as t
+import bz2
+
 
 MILLION = 1_000_000
 
 
-@test("Not a very good test")
-def _():
-    assert words(75)
+@fixture
+def word_list() -> t.List:
+    with resources.as_file(resources.files(data)) as f:
+        with open((f / "words.bz"), "rb") as bz_file:
+            return bz2.decompress(bz_file.read()).decode("utf-8").split()
 
 
-@test("Test for id collisions")
+@test("Test for ue_id collisions")
 def _():
     ids = [ue_id() for i in range(MILLION)]
     assert len(list(set(ids))) == len(ids)
+
+
+@test("Test for word hashing collisions")
+def _(word_list=word_list):
+
+    known = defaultdict(list)
+
+    [known[hash_word(w)].append(w) for w in word_list]
+    collisions = [v for k, v in known.items() if len(v) > 1]
+
+    collision_percent = (len(collisions) * 1.00 / len(word_list)) * 100
+    assert collision_percent < 0.02
